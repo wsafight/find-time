0a644e384c7263622ccbe102ea5ec450b5193ae0
jump-and-jump <984292420@qq.com>
2025-01-03 16:17:31 +0800

    chore: change dep

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    alias(libs.plugins.kotlinMultiplatform)
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "17"
+        }
+    }
+    sourceSets {
+        val jvmMain by getting {
+            kotlin.srcDirs("src/jvmMain/kotlin")
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                api(compose.runtime)
+                api(compose.foundation)
+                api(compose.material)
+                api(compose.ui)
+                api(compose.materialIconsExtended)
+
+                implementation(project(":shared"))
+                implementation(project(":shared-ui"))
+            }
+        }
+    }
+}
+
+compose.desktop {
+    application() {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "FindTime"
+            macOS {
+                bundleID = "com.wsafight.findtime"
+            }
+        }
+    }
+}
